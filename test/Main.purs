91d551dae83714ea1294385e43c9ac65a0196f76
Cole Haus <colehaus@cryptolab.net>
2018-04-05 12:25:13 -0700

    Add test runner

--- test/Main.purs
+++ test/Main.purs
@@ -1,9 +1,22 @@
 module Test.Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "You should add some tests."
+import Control.Monad.Eff (Eff)
+import Test.QuickCheck (arbitrary, (/==), (===))
+import Test.Spec (describe, it)
+import Test.Spec.QuickCheck (QCRunnerEffects, quickCheck)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (run)
+
+main :: Eff (QCRunnerEffects ()) Unit
+main = run [consoleReporter] do
+  describe "Math" do
+    it "works" $
+      quickCheck $ do
+        n <- arbitrary
+        pure $ (n * 2 / 2) === n
+    it "works again" $ do
+      quickCheck $ do
+        n <- arbitrary
+        pure $ ((n + 1) * 2) /== n
