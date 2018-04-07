b52b457ce28f4b43c6efd44be4d5a218e1365025
Cole Haus <colehaus@cryptolab.net>
2018-04-07 03:37:36 -0700

    Add NonEmpty.Indexed

--- src/Math/Probability/Information/Value/Internal.purs
+++ src/Math/Probability/Information/Value/Internal.purs
@@ -1,10 +1,11 @@
 module Math.Probability.Information.Value.Internal where
 
 import Data.Map (Map)
-import Data.NonEmpty (NonEmpty)
+import Data.NonEmpty.Indexed (NonEmpty)
 import Data.Tuple (Tuple)
 import Math.Probability (Dist)
 
-type SimpleDecisionTree choice result = NonEmpty (Map choice) (Dist result)
+
+type SimpleDecisionTree choice result = NonEmpty Map choice (Dist result)
 newtype InvestigationAndDecisionTree finding choice result =
   MkInvestigationAndDecisionTree (Dist (Tuple finding (SimpleDecisionTree choice result)))
